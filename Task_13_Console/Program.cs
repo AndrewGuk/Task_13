3b163be518ec1e6dec55f3f7629ec0acc4f2f4f6
Andrei Huk <belakolala1@gmail.com>
2022-04-06 21:06:46 +0300

    Добавьте файлы проекта.

---
+++ Task_13_Console/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Types;
+
+namespace Task_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t1 = new Ranges(name: "audi", storeName: "AudiShop");
+            var t2 = new Ranges(name: "mercedes", storeName: "MBshop");
+            var t3 = new Ranges(name: "bmw", storeName: "bmwShop");
+            var t4 = new Ranges(name: "mazda", storeName: "MazdaShop");
+            var t5 = new Ranges(name: "mitsubishi", storeName: "MitsubishiMBshop");
+
+
+            var product = new Product();
+            product.AddProduct(t1);
+            product.AddProduct(t2);
+            product.AddProduct(t3);
+            product.AddProduct(t4);
+            product.AddProduct(t5);
+
+            Console.WriteLine("\tNot sort list:");
+            product.Print();
+
+            product.Soring();
+
+            Console.WriteLine("\tSort list:");
+            product.Print();
+
+            product.FindShop(Console.ReadLine());
+
+
+        }
+    }
+}
---
+++ Types/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace Types
+{
+    public struct Ranges
+    {
+        public string Name { get; set; }
+        public string StoreName { get; set; }
+        public Ranges(string name, string storeName)
+        {
+            Name = name;
+            StoreName = storeName;
+        }
+
+    }
+    public struct Product
+    {
+        public Product()
+        {
+        }
+        List<Ranges> productList = new List<Ranges>();
+        public void AddProduct(Ranges product)
+        {
+            productList.Add(product);
+        }
+        public void Soring()
+        {
+            productList = productList.OrderBy(x => x.StoreName).ToList();
+        }
+        public void Print()
+        {
+            foreach (Ranges product in productList)
+                Console.WriteLine($"Product: {product.Name}\n Shop: {product.StoreName}");
+        }
+        public void FindShop(string nameshop)
+        {
+            for (int i = 0; i < productList.Count; i++)
+            {
+                if (productList[i].StoreName.ToLower() == nameshop.ToLower())
+                {
+                    Console.WriteLine($"Shop <{productList[i].StoreName}> has <{productList[i].Name}>");
+                    return;
+                }
+            }
+            throw new UserExeption(nameshop);
+        }
+    }
+    public class UserExeption : ApplicationException
+    {
+        public UserExeption(string x)
+        {
+            Console.WriteLine($"The store with the name <{x}> does not exist.");
+        }
+    }
+}
